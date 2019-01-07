a514c3dafb11417b410214912fe404612870018c
hjshin <shj950505@naver.com>
2019-01-07 17:23:43 +0900

    Initial commit 2019-01-07 신현주

---
+++ app/src/main/java/com/example/administrator/almin/join_member.java
@@ -0,0 +1,13 @@
+package com.example.administrator.almin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class join_member extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_member);
+    }
+}
